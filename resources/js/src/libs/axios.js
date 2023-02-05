abc13eacee9cea8afcaad9a6a03801915324873c
arnonr <arnon.r@tgde.kmutnb.ac.th>
2023-02-05 20:19:40 +0700

    axios

--- resources/js/src/libs/axios.js
+++ resources/js/src/libs/axios.js
@@ -3,10 +3,16 @@ import Vue from 'vue'
 // axios
 import axios from 'axios'
 
+let baseUrl = "http://143.198.208.110:8105/api";
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1"){
+  baseUrl = "http://localhost:8105/api/";
+}
+
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: 'http://localhost:8105/api/',
+  baseURL: baseUrl,
+  // baseURL: 'http://localhost:8105/api/',
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 })
